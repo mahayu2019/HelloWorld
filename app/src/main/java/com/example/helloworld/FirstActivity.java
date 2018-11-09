e12556843e7a51ddff9a763bab9eb8d63c8c9338
mahayu2019 <2353987275@qq.com>
2018-11-09 14:15:31 +0800

    2.3继续

--- app/src/main/java/com/example/helloworld/FirstActivity.java
+++ app/src/main/java/com/example/helloworld/FirstActivity.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,7 +19,12 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "按钮1", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(FirstActivity.this, "按钮1", Toast.LENGTH_SHORT).show();
+               // Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+              //  startActivity(intent);
+                Intent intent=new Intent("com.example.activitytest.ACTION_START");
+                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+                startActivity(intent);
             }
         });
     }
