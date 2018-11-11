feac25dec50a92609c70643fbb69e18e095220d0
mahayu2019 <2353987275@qq.com>
2018-11-11 20:32:46 +0800

    2.3继续

--- app/src/main/java/com/example/helloworld/FirstActivity.java
+++ app/src/main/java/com/example/helloworld/FirstActivity.java
@@ -1,8 +1,11 @@
 package com.example.helloworld;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +13,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +23,23 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Toast.makeText(FirstActivity.this, "按钮1", Toast.LENGTH_SHORT).show();
-               // Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
-              //  startActivity(intent);
-                Intent intent=new Intent("com.example.activitytest.ACTION_START");
-                intent.addCategory("com.example.activitytest.MY_CATEGORY");
-                startActivity(intent);
+                // Toast.makeText(FirstActivity.this, "按钮1", Toast.LENGTH_SHORT).show();
+                // Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                //  startActivity(intent);
+//                Intent intent = new Intent("ACTION_START"); //隐式调用指定的action
+//                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+//                startActivity(intent);
+
+//                Intent intent = new Intent(Intent.ACTION_VIEW);  //系统内置动作
+//                intent.setData(Uri.parse("tel:123456"));
+//                startActivity(intent);
+
+//                String data = "参数传递";
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                intent.putExtra("extra_data", data);
+//                startActivity(intent);
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
     }
@@ -48,5 +63,18 @@ public class FirstActivity extends AppCompatActivity {
         }
         return true;
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("第一屏", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
--- app/src/main/java/com/example/helloworld/SecondActivity.java
+++ app/src/main/java/com/example/helloworld/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+//        Intent intent = getIntent();
+//        String data = intent.getStringExtra("extra_data");
+//        Log.d("第二活动版", data);
+        Button bt2 = (Button) findViewById(R.id.button2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "返回第一屏");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "返回第一屏");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
